38b1e0df7e90f4073ad59ceb1dd5e0e6672471ef
salinov <salinovsciy@gmail.com>
2020-10-20 21:50:48 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let chars = 0;
+  bracketsConfig.forEach((item, numb) => bracketsConfig[numb] = String(item[0] + item[1]));
+  while (true) {
+    bracketsConfig.forEach((item) => str = str.replace(item, ''));
+    if (str === '') return true;
+    if (chars === str.length) return false;
+    chars = str.length;
+  }
 }
